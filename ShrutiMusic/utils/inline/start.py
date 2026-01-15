d927a66e9b3c66887f6ec5ae5539188ce1edc151
TEAM-NOBITA <nobitasir251@gmail.com>
2026-01-15 11:14:15 +0530

    Shruti Muzic

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -2,35 +2,37 @@ from pyrogram.types import InlineKeyboardButton
 import config
 from ShrutiMusic import app
 
+
 def about_panel():
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-    ]
-    return buttons
-
-
-def about_panel():
-    buttons = [
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT
+            ),
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                user_id=config.OWNER_ID
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                callback_data="settings_back_helper"
+            ),
         ],
     ]
     return buttons
+    
