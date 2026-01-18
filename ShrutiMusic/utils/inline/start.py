64b6d9e38a2937837fe04f9e95d4c9cde908d5d2
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2026-01-18 20:37:05 +0530

    🥀ᴀʏᴜsʜ🥀

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
-                callback_data="settings_back_helper"
+                callback_data="help_page_1"
             )
         ],
         [
